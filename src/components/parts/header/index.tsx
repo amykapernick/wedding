0335898543beba876d0a36aa978594a2bc9a641b
Amy Kapernick <amy.kapernick@makerx.com.au>
2024-01-10 11:26:46 +0800

    hide rsvp for now

--- src/components/parts/header/index.tsx
+++ src/components/parts/header/index.tsx
@@ -7,7 +7,12 @@ const Header = () =>
 	return (
 		<header className={styles.header}>
 			<h1 className={styles.heading}>Daniel & Amy <span>are getting married</span></h1>
-			<Image src={HeaderImage} alt="Daniel and Amy" className={styles.image} placeholder="blur" />
+			<Image
+				src={HeaderImage}
+				alt="Daniel and Amy"
+				className={styles.image}
+				placeholder="blur"
+			/>
 		</header>
 	)
 }
