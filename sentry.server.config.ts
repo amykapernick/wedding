3116001720d8d9d5ee2862cc5973306e1aec0120
Amy Kapernick <amy.kapernick@makerx.com.au>
2024-01-19 14:04:23 +0800

    added sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://6576e20ce71d209daf053e57ce1e8302@o1145095.ingest.sentry.io/4506596412620800",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
