58473bfa1afd34817511771da8657b13289117dd
Amy Kapernick <amy.kapernick@makerx.com.au>
2024-02-11 21:45:27 +0800

    added tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout ({
 			<html lang="en-AU">
 				<head>
 					<title>{metadata.title}</title>
+					<script src="https://cdn.usefathom.com/script.js" data-site={process.env.NEXT_PUBLIC_FATHOM_SITE} defer></script>
 				</head>
 				<body>
 					<Header />
