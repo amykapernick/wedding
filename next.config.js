ca59cb4a998c55dd5f045f8d2ed758179b2e16b0
Amy Kapernick <amy@kapers.dev>
2024-07-04 17:31:12 +0800

    working on sentry

--- next.config.js
+++ next.config.js
@@ -5,48 +5,48 @@
  **/
 
 module.exports = async (phase, { defaultConfig }) => {
-	const nextConfig = {
-		...defaultConfig,
-		reactStrictMode: true,
-		images: {
-			remotePatterns: [
-				{
-					protocol: 'https',
-					hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
-					port: '',
-				},
-			],
-		},
-		webpack(config) {
-			// Grab the existing rule that handles SVG imports
-			const fileLoaderRule = config.module.rules.find((rule) =>
-				rule.test?.test?.('.svg'),
-			)
+  const nextConfig = {
+    ...defaultConfig,
+    reactStrictMode: true,
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+          port: '',
+        },
+      ],
+    },
+    webpack(config) {
+      // Grab the existing rule that handles SVG imports
+      const fileLoaderRule = config.module.rules.find((rule) =>
+        rule.test?.test?.('.svg'),
+      )
 
-			config.module.rules.push(
-				// Reapply the existing rule, but only for svg imports ending in ?url
-				{
-					...fileLoaderRule,
-					test: /\.svg$/i,
-					resourceQuery: /url/, // *.svg?url
-				},
-				// Convert all other *.svg imports to React components
-				{
-					test: /\.svg$/i,
-					issuer: fileLoaderRule.issuer,
-					resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
-					use: ['@svgr/webpack'],
-				},
-			)
+      config.module.rules.push(
+        // Reapply the existing rule, but only for svg imports ending in ?url
+        {
+          ...fileLoaderRule,
+          test: /\.svg$/i,
+          resourceQuery: /url/, // *.svg?url
+        },
+        // Convert all other *.svg imports to React components
+        {
+          test: /\.svg$/i,
+          issuer: fileLoaderRule.issuer,
+          resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+          use: ['@svgr/webpack'],
+        },
+      )
 
-			// Modify the file loader rule to ignore *.svg, since we have it handled now.
-			fileLoaderRule.exclude = /\.svg$/i
+      // Modify the file loader rule to ignore *.svg, since we have it handled now.
+      fileLoaderRule.exclude = /\.svg$/i
 
-			return config
-		}
-	}
+      return config
+    }
+  }
 
-	return nextConfig
+  return nextConfig
 }
 
 // Injected content via Sentry wizard below
@@ -59,22 +59,22 @@ module.exports = withSentryConfig(
     // For all available options, see:
     // https://github.com/getsentry/sentry-webpack-plugin#options
 
-    // Suppresses source map uploading logs during build
-    silent: true,
     org: "amys-kapers",
     project: "wedding-website",
-  },
-  {
+
+    // Only print logs for uploading source maps in CI
+    silent: !process.env.CI,
+
     // For all available options, see:
     // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
     // Upload a larger set of source maps for prettier stack traces (increases build time)
     widenClientFileUpload: true,
 
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
-
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+    // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+    // This can increase your server load as well as your hosting bill.
+    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+    // side errors will fail.
     tunnelRoute: "/monitoring",
 
     // Hides source maps from generated client bundles
@@ -83,7 +83,7 @@ module.exports = withSentryConfig(
     // Automatically tree-shake Sentry logger statements to reduce bundle size
     disableLogger: true,
 
-    // Enables automatic instrumentation of Vercel Cron Monitors.
+    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
     // See the following for more information:
     // https://docs.sentry.io/product/crons/
     // https://vercel.com/docs/cron-jobs
