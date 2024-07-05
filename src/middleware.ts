97137ada63d9f5c8acce00118f1d2252256474cf
Amy Kapernick <amy@kapers.dev>
2024-07-05 09:18:07 +0800

    trying middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,5 +11,5 @@ export default clerkMiddleware((auth, req) =>
 });
 
 export const config = {
-	matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)', '/(?!monitoring)'],
+	matcher: ['/((?!.*\\..*|_next|monitoring).*)', '/', '/(api|trpc)(.*)'],
 };
