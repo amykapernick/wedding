28a1e9947032705838903cf92276b1292c31abc9
Amy Kapernick <amy@kapers.dev>
2024-07-05 08:52:13 +0800

    added sentry

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import * as Sentry from "@sentry/nextjs";
 import Error from "next/error";
 import { useEffect } from "react";
 
 export default function GlobalError({ error }: Readonly<{ error: any }>) {
-	useEffect(() => {}, [error]);
+	useEffect(() => {
+		Sentry.captureException(error);
+	}, [error]);
 
 	return (
 		<html lang="en-AU">
