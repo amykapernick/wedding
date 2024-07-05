7c13d3d1b992dc9bf84b045cfedfa4923695aa7f
Amy Kapernick <amy@kapers.dev>
2024-07-05 09:09:07 +0800

    added other errors

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -2,6 +2,7 @@
 
 import { Client } from '@notionhq/client';
 import { Person } from '@ts/people';
+import * as Sentry from "@sentry/nextjs";
 import { trackEvent } from 'fathom-client';
 
 const notionFields: Record<string, string> = {
@@ -102,6 +103,8 @@ export async function submit (guest: string, formData: FormData)
 		{
 			console.log({ err })
 
+			Sentry.captureException(err)
+
 			return err
 		})
 
@@ -132,6 +135,8 @@ export async function updateGift (gift: string, purchased: number)
 		.catch(err =>
 		{
 			console.log({ err })
+
+			Sentry.captureException(err)
 
 			return err
 		})
