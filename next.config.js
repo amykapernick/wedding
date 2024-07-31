2d1de3bcbfa6330d7f216a09d833dacb3ee10533
Amy Kapernick <amy@kapers.dev>
2024-07-31 16:32:40 +0800

    trying again

--- next.config.js
+++ next.config.js
@@ -17,9 +17,10 @@ module.exports = async (phase, { defaultConfig }) => {
         },
       ],
     },
-    // experimental: {
-    //   instrumentationHook: true
-    // },
+
+    typescript: {
+      ignoreBuildErrors: true,
+    },
     webpack(config) {
       // Grab the existing rule that handles SVG imports
       const fileLoaderRule = config.module.rules.find((rule) =>
